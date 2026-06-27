277dfa4658928c563fc10fbadb75d760301664ea
Maxim-Privalov <64764258+Maxim-Privalov@users.noreply.github.com>
2026-06-27 17:43:03 +1000

    Revert "Feature/add extra screens"

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,144 +1,40 @@
 using Godot;
+using System;
 
 public partial class Player : CharacterBody2D
 {
-	[Export]
-	public float Speed = 650f;
-
-	[Export]
-	public float JumpVelocity = -850f;
-
-	[Export]
-	public float Gravity = 1200f;
-
-	[Export]
-	public float Acceleration = 1800f;
-
-	[Export]
-	public float Friction = 2200f;
-
-	[Export]
-	public int DropThroughPlatformLayer = 2;
-
-	[Export]
-	public float DropThroughTime = 0.25f;
-
-	[Export]
-	public float DropVelocity = 80f;
-
-	private bool _isDroppingThroughPlatform = false;
-
-	private AnimatedSprite2D _animatedSprite;
-
-	public override void _Ready()
-	{
-		_animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-	}
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
+		// Add the gravity.
 		if (!IsOnFloor())
 		{
-			velocity.Y += Gravity * (float)delta;
+			velocity += GetGravity() * (float)delta;
 		}
 
-		float direction = 0;
-
-		if (Input.IsActionPressed("moveLeft"))
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 		{
-			direction -= 1;
-			_animatedSprite.FlipH = true;
+			velocity.Y = JumpVelocity;
 		}
 
-		if (Input.IsActionPressed("moveRight"))
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
 		{
-			direction += 1;
-			_animatedSprite.FlipH = false;
-		}
-
-		if (direction != 0)
-		{
-			
-			velocity.X = Mathf.MoveToward(
-				velocity.X,
-				direction * Speed,
-				Acceleration * (float)delta
-			);
+			velocity.X = direction.X * Speed;
 		}
 		else
 		{
-			
-			velocity.X = Mathf.MoveToward(
-				velocity.X,
-				0,
-				Friction * (float)delta
-			);
-		}
-		if (Input.IsActionPressed("moveDown"))
-			{
-				StartDropThroughPlatform();
-			}
-		if (Input.IsActionJustPressed("moveUp") && IsOnFloor())
-		{
-				velocity.Y = JumpVelocity;	
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
-
-		UpdateAnimation(direction);
-	}
-
-	private void UpdateAnimation(float direction)
-	{
-			if (!IsOnFloor())
-		{
-			if (Velocity.Y < 0)
-			{
-				PlayAnimation("jump");
-			}
-			else
-			{
-				PlayAnimation("drop");
-			}
-		}
-		else if (direction != 0)
-		{
-			PlayAnimation("run");
-		}
-		
-		else
-		{
-			PlayAnimation("default");
-		}
-	}
-
-	private void PlayAnimation(string animationName)
-	{
-		if (_animatedSprite.Animation != animationName)
-		{
-			_animatedSprite.Play(animationName);
-		}
-	}
-
-	private async void StartDropThroughPlatform()
-	{
-		if (_isDroppingThroughPlatform)
-			return;
-
-		_isDroppingThroughPlatform = true;
-
-		SetCollisionMaskValue(DropThroughPlatformLayer, false);
-
-		await ToSignal(
-			GetTree().CreateTimer(DropThroughTime),
-			SceneTreeTimer.SignalName.Timeout
-		);
-
-		SetCollisionMaskValue(DropThroughPlatformLayer, true);
-
-		_isDroppingThroughPlatform = false;
 	}
 }
